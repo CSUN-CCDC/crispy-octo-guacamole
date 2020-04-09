04dfc06b5e14f0639a47515c7ed1d8294ea42606
username <notarealemail.com>
2020-04-08 18:14:32 -0700

    auditing

--- Wangblows.bat
+++ Wangblows.bat
@@ -22,7 +22,7 @@ echo 8. System Integrity Scan
 echo 9. Powershell rootkit detection
 
 CHOICE /C 123456789 /M "Enter your choice:"
-if ERRORLEVEL 19 goto Nineteen
+if ERRORLEVEL 10 goto Ten
 if ERRORLEVEL 9 goto Nine
 if ERRORLEVEL 8 goto Eight
 if ERRORLEVEL 7 goto Seven
@@ -424,5 +424,56 @@ echo "POWERSHELL ROOTKIT DETECTION WITH MALWAREBYTES ROOTKIT BETA (Requires powe
 REM Downloads MalwareBytes scan file
 powershell Invoke-WebRequest -OutFile MBRTKit.exe https://data-cdn.mbamupdates.com/web/mbar-1.10.3.1001.exe
 MBRTKit.exe
+goto MENU
+
+
+:Ten
+auditpol /set /subcatergory: "Detailed File Share" /success:enable /failure:enable
+auditpol /set /subcatergory: "File System" /success:enable /failure:enable
+auditpol /set /subcatergory: "Security System Extension" /success:enable /failure:enable
+auditpol /set /subcatergory: "System Integrity" /success:enable /failure:enable
+auditpol /set /subcatergory: "Security State Change" /success:enable /failure:enable
+auditpol /set /subcatergory: "Other System Events" /success:enable /failure:enable
+auditpol /set /subcatergory: "System Integrity" /success:enable /failure:enable
+auditpol /set /subcatergory: "Logon" /success:enable /failure:enable
+auditpol /set /subcatergory: "Logoff" /success:enable /failure:enable
+auditpol /set /subcatergory: "Account Lockout" /success:enable /failure:enable
+auditpol /set /subcatergory: "Other Logon/Logoff Events" /success:enable /failure:enable
+auditpol /set /subcatergory: "Network Policy Server" /success:enable /failure:enable
+auditpol /set /subcatergory: "Registry" /success:enable /failure:enable
+auditpol /set /subcatergory: "SAM" /success:enable /failure:enable
+auditpol /set /subcatergory: "Certification Services" /success:enable /failure:enable
+auditpol /set /subcatergory: "Application Generated" /success:enable /failure:enable
+auditpol /set /subcatergory: "Handle Manipulation" /success:enable /failure:enable
+auditpol /set /subcatergory: "Filtering Platform Packet Drop" /success:enable /failure:enable
+auditpol /set /subcatergory: "Filtering Platform Connection" /success:enable /failure:enable
+auditpol /set /subcatergory: "Other Object Access Events" /success:enable /failure:enable
+auditpol /set /subcatergory: "Detailed File Share" /success:enable /failure:enable
+auditpol /set /subcatergory: "Sensitive Privilege" /success:enable /failure:enable
+auditpol /set /subcatergory: "Non Sensitive Privilege" /success:enable /failure:enable
+auditpol /set /subcatergory: "Other Privilege Use Events" /success:enable /failure:enable
+auditpol /set /subcatergory: "Process Termination" /success:enable /failure:enable
+auditpol /set /subcatergory: "DPAPI Activity" /success:enable /failure:enable
+auditpol /set /subcatergory: "RPC Activity" /success:enable /failure:enable
+auditpol /set /subcatergory: "Process Creation" /success:enable /failure:enable
+auditpol /set /subcatergory: "Audit Policy Change" /success:enable /failure:enable
+auditpol /set /subcatergory: "Authentication Policy Change" /success:enable /failure:enable
+auditpol /set /subcatergory: "MPSSVC Rule-Level Policy" /success:enable /failure:enable
+auditpol /set /subcatergory: "Filtering Platform Policy" /success:enable /failure:enable
+auditpol /set /subcatergory: "Other Policy Change Events" /success:enable /failure:enable
+auditpol /set /subcatergory: "User Account Management" /success:enable /failure:enable
+auditpol /set /subcatergory: "Computer Account Management" /success:enable /failure:enable
+auditpol /set /subcatergory: "Security Group Management" /success:enable /failure:enable
+auditpol /set /subcatergory: "Distribution Group" /success:enable /failure:enable
+auditpol /set /subcatergory: "Application Group Management" /success:enable /failure:enable
+auditpol /set /subcatergory: "Other Account Management Events" /success:enable /failure:enable
+auditpol /set /subcatergory: "Directory Service Changes" /success:enable /failure:enable
+auditpol /set /subcatergory: "Directory Service Replications" /success:enable /failure:enable
+auditpol /set /subcatergory: "Detailed Directory Service Replications" /success:enable /failure:enable
+auditpol /set /subcatergory: "Directory Service Access" /success:enable /failure:enable
+auditpol /set /subcatergory: "Kerberos Service Ticket Operations" /success:enable /failure:enable
+auditpol /set /subcatergory: "Other Account Logon Events" /success:enable /failure:enable
+auditpol /set /subcatergory: "Kerberos Authentication Service" /success:enable /failure:enable
+auditpol /set /subcatergory: "Credential Validation" /success:enable /failure:enable
 goto MENU
 PAUSE
