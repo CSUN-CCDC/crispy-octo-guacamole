f7afbdcffd4c1fb00899fe7dfb45cb1e557c3c8c
revrtwo <yorazor128@gmail.com>
2023-03-30 12:52:35 -0700

    Directory change for Isaiah
    directory change for Isaiah

--- WMFUpdates.ps1
+++
@@ -1,9 +0,0 @@
-# Check for WMF updates
-$wmfUpdates = Get-HotFix -Description "Windows Management Framework" | Where-Object {$_.HotFixID -notlike "*KB*"}
-
-if ($wmfUpdates) {
-    Write-Host "WMF updates available:" -ForegroundColor Green
-    $wmfUpdates | Format-Table -AutoSize
-} else {
-    Write-Host "No WMF updates available." -ForegroundColor Yellow
-}
