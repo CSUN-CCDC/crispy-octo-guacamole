8cbf1b636b1d787fa612acfaeb0fbe606d0ed15d
Arian-D <ArianXDehghani@gmail.com>
2023-08-19 13:03:37 -0700

    Add -a flag for appending while using `tee`

--- Linux/scripts/inventory.sh
+++ Linux/scripts/inventory.sh
@@ -6,11 +6,11 @@ FILE="inventory.txt"
 
 # Get OS info
 if [ $(command -v hostnamectl) ]; then
-    hostnamectl | tee $FILE
+    hostnamectl | tee -a $FILE
 elif [ -r /etc/os-release ]; then
-    cat /etc/os-release | tee $FILE
+    cat /etc/os-release | tee -a $FILE
 elif [ -r /etc/lsb-release ]; then
-    cat /etc/lsb-release | tee $FILE
+    cat /etc/lsb-release | tee -a $FILE
 else
     echo "Was not able to get OS info"
     exit 1
@@ -18,9 +18,9 @@ fi
 
 # Get IP information
 if [ $(command -v ip) ]; then
-    ip -brief address | tee $FILE
+    ip -brief address | tee -a $FILE
 elif [ $(command -v ifconfig) ]; then
-    ifconfig | tee $FILE
+    ifconfig | tee -a $FILE
 else
     echo "Neither ip nor ifconfig are installed; quitting."
     exit 1
@@ -28,19 +28,19 @@ fi
 
 # Get open ports
 if [ $(command -v ss) ]; then
-    ss -lnptu | tee $FILE
+    ss -lnptu | tee -a $FILE
 elif [ $(command -v netstat) ]; then
-    netstat -tulpn | tee $FILE
+    netstat -tulpn | tee -a $FILE
 elif [ $(command -v nmap) ]; then
-    nmap -sV localhost | tee $FILE
+    nmap -sV localhost | tee -a $FILE
 else
     echo "no port scan tool is available; quitting"
     exit 1
 fi
 
 # Get users
-echo "Users: " | tee $FILE
-egrep -v 'nologin$' /etc/passwd | cut -d: -f1 | tee $FILE
+echo "Users: " | tee -a $FILE
+egrep -v 'nologin$' /etc/passwd | cut -d: -f1 | tee -a $FILE
 
 # Experimental uploading
 if [ $(command -v curl) ]; then
